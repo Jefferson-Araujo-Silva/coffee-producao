c2c5595747946d26254dc5dbc71d4cbd800d8f36
Jefferson-Araujo-Silva <jefferson.silva@sptech.school>
2022-12-06 19:07:57 -0300

    a

--- site/srcjs/models/usuarioModel.js
+++ site/srcjs/models/usuarioModel.js
@@ -37,7 +37,7 @@ function plantar(especie, estado, cidade, tamanho, fkCliente) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO PLANTACAO (tipo, UF, cidade, Tamanho_m2, fkCliente) VALUES ('${especie}', '${estado}', '${cidade}', '${tamanho}', '${fkCliente}');
+        INSERT INTO PLANTACAO (tipo, UF, cidade, Tamanho_m2, fkCliente) VALUES ('${especie}', '${estado}', '${cidade}', ${tamanho}, ${fkCliente});
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
